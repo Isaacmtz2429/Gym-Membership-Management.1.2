c0a14a544486c2f840a636220f029f6f46f7e696
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-03-02 22:16:42 -0800

    created derived class and created added in statments and validations

--- Gym Membership Management.1.2/Member.cs
+++ Gym Membership Management.1.2/Member.cs
@@ -8,5 +8,25 @@ namespace Gym_Membership_Management._1._2
 {
     class Member : MemberBase
     {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Name can't be empty");
+                }
+            }
+        }
+        public override void DisplayDetails()
+        {
+            Console.WriteLine($"Member Name: {Name}");
+        }
     }
 }
